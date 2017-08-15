a7844f8310ce7114feb8586b1ae8fcfd4da8bda8
nathants <me@nathants.com>
2017-08-14 22:41:13 -0700

    iterating on prototype

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    version="0.0.1",
+    license='mit',
+    name='s4',
+    author='nathan todd-stone',
+    author_email='me@nathants.com',
+    url='http://github.com/nathants/s4',
+    packages=['s4'],
+    install_requires=['argh==0.26.1'],
+    description='stupid simple storage service',
+)
