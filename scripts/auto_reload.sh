1dd74c55f4a810d58dbcb6761fde3620feefcfc7
nathants <me@nathants.com>
2020-06-26 20:20:35 -0700

    reload takes name as a param and cleans up old entr instances

--- scripts/auto_reload.sh
+++ scripts/auto_reload.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -eou pipefail
 
+name=$1
+
+if ! which aws-ec2-new &>/dev/null; then
+    echo fatal: need to install https://github.com/nathants/cli-aws
+    exit 1
+fi
+
 cd $(dirname $(dirname $0))
 
 # kill any running servers
-aws-ec2-ssh s4-cluster -yc "killall -r pypy3"
+aws-ec2-ssh $name -yc "killall -r pypy3 || true"
+aws-ec2-ssh $name -yc "killall -r entr || true"
 
 # setup the server reloader
-aws-ec2-ssh s4-cluster --no-tty -yc "cd /mnt; ((find ~/s4 -type f -name '*.py' -o -name '*.sh' | entr -r s4-server) &> s4.log </dev/null) &"
+aws-ec2-ssh $name --no-tty -yc "cd /mnt; ((find ~/s4 -type f -name '*.py' -o -name '*.sh' | entr -r s4-server) &> s4.log </dev/null) &"
 
 # run the local file pusher
-find -type f -name '*.py' -o -name '*.sh' | entr aws-ec2-rsync . :s4/ s4-cluster -y
+find -type f -name '*.py' -o -name '*.sh' | entr aws-ec2-rsync . :s4/ $name -y
