9a162b94000ee17cdeffc1bb217837ed7a5ee743
nathants <me@nathants.com>
2020-06-29 17:05:20 -0700

    add nyc tax example

---
+++ examples/nyc_taxi_groupby_passengers_and_count.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -euo pipefail
+
+echo; echo cluster health:
+s4 health
+
+echo; echo dataset:
+prefix='s3://nyc-tlc/trip data/'
+keys=$(aws s3 ls "$prefix" \
+           | grep yellow \
+           | awk '{print $NF}' \
+           | while read key; do echo $prefix$key; done)
+echo "$keys"
+
+echo; echo headers:
+(aws s3 cp "s3://nyc-tlc/trip data/yellow_tripdata_2019-12.csv" - || true) 2>/dev/null | head -n1 | tr , '\n' | cat -n
+
+echo; echo 01 put inputs keys
+if ! s4 ls s4://bucket/01 >/dev/null; then (
+    IFS=$'\n'
+    i=0
+    for key in $keys; do
+        echo $key | s4 cp - s4://bucket/01_inputs/$(printf "%03d" $i)
+        i=$((i+1))
+    done
+) fi
+
+echo; echo 02 fetch csv and drop header
+if ! s4 ls s4://bucket/02 >/dev/null; then
+    time s4 map \
+         s4://bucket/01_inputs/ \
+         s4://bucket/02_csv/ \
+         'cat - > url && aws s3 cp "$(cat url)" - | tail -n+2'
+fi
+
+echo; echo 03 select, filter, and convert to bsv
+if ! s4 ls s4://bucket/03 >/dev/null; then
+    time s4 map \
+         s4://bucket/02_csv/ \
+         s4://bucket/03_bsv/ \
+         'cut -d, -f2,3,4,5,10,17 | awk -F, NF==6 | bsv' # fields: 2,3,4,5,10,17 = pickup-date, dropoff-date, passenger-count, distance, payment_type, total_amount
+fi
+
+s4 rm -r s4://bucket/04
+s4 rm -r s4://bucket/05
+s4 rm -r s4://bucket/06
+s4 rm -r s4://bucket/07
+
+echo; echo 04 groupby passenger count
+if ! s4 ls s4://bucket/04 >/dev/null; then
+    time s4 map-to-n \
+         s4://bucket/03_bsv/ \
+         s4://bucket/04_grouped/ \
+         'bcut 3 | bbucket 256 | bpartition 256'
+fi
+
+echo; echo 05 sort groups
+if ! s4 ls s4://bucket/05 >/dev/null; then
+    time s4 map \
+         s4://bucket/04_grouped/ \
+         s4://bucket/05_sorted/ \
+         'bsort'
+fi
+
+echo; echo 06 merge sorted groups
+if ! s4 ls s4://bucket/06 >/dev/null; then
+    time s4 map-from-n \
+         s4://bucket/05_sorted/ \
+         s4://bucket/06_merged/ \
+         'bmerge'
+fi
+
+echo; echo 07 count and convert to csv
+if ! s4 ls s4://bucket/07 >/dev/null; then
+    time s4 map \
+         s4://bucket/06_merged/ \
+         s4://bucket/07_counts/ \
+         'bcounteach | bschema *,u64:a | csv'
+fi
+
+echo; echo fetch the results; echo
+(
+    echo num-passengers num-trips
+    s4 ls s4://bucket/07 -r \
+        | awk '{print $NF}' \
+        | while read key; do s4 eval s4://bucket/$key cat; done \
+        | grep -v ^$ \
+        | tr , ' ' \
+        | sort -nrk 2
+) | column -t
