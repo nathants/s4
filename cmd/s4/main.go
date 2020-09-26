c35858cac3a483dae5bdeab4c9a0678f9c12c6a4
nathants <me@nathants.com>
2020-09-25 18:49:55 -0700

    scaffold cli

--- cmd/s4/main.go
+++ cmd/s4/main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+func Eval() {
+	fmt.Println("eval")
+}
+
+func Ls() {
+}
+
+func Cp() {
+}
+
+func Config() {
+}
+
+func Health() {
+}
+
+func usage() {
+	fmt.Println("usage: ")
+	os.Exit(1)
+}
+
 func main() {
-	fmt.Println("s4")
+	if len(os.Args) < 2 {
+		usage()
+	}
+	switch os.Args[1] {
+	case "eval":
+		Eval()
+	case "ls":
+		Ls()
+	case "cp":
+		Cp()
+	case "config":
+		Config()
+	case "health":
+		Health()
+	default:
+		usage()
+	}
 }
